1ab9fc482af54ebe1a47a8b904038a22a76ff23f
Aditya Shekhar Salugu Sai <adityashekhar146@gmail.com>
2022-07-30 23:47:56 +0530

    Add initial site based on NextJS and TailwindCSS template

---
+++ components/header/header.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Header: React.FC = () => {
+
+  return (
+    <nav className="bg-gray-50 w-full mt-0 flex flex-row">
+      <div className="flex flex-row items-center">
+        <Image src="/images/cloud-refresh.png" height={64} width={64} alt="" 
+          className=""
+        />
+        <h2 className="text-3xl font-bold mx-3">Discover Next</h2>
+      </div>
+      <ul className="mx-10 sm:hidden md:inline-flex flex-row items-start">
+        <li className="header-link"><Link href="/">Home</Link></li>
+        <li className="header-link"><Link href="/about">About Us</Link></li>
+      </ul>
+      
+    </nav>
+  );
+}
+
+export default Header;
